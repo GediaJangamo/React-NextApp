85b123055b1db5b1e6908c3bce759f0e0f7e8d47
Gedia Jangamo <gedyahgennyfah@gmail.com>
2023-12-29 17:09:28 +0200

    feat: working
    working

--- inicial-app/src/app/components/hooks/ImagemAleatoria.tsx
+++ inicial-app/src/app/components/hooks/ImagemAleatoria.tsx
@@ -1,7 +1,32 @@
-export default function ImagemAleatoria(){
-    return(
-        <div>
-            Imagem Aleatoria
+import Image from "next/image"
+import { useState } from "react"
+
+export default function ImagemAleatoria() {
+
+//   const [pesquisa, setPesquisa] = useState<string>('Abstract')
+   
+  
+
+    function renderizarBotao(valor: string) {
+        return (
+            <button className={`px-4 py-2  bg-blue-600 rounded-md`}
+            // onClick={()=> setPesquisa(valor)}
+            >
+                {valor}
+            </button>
+        )
+    }
+
+    return (
+        <div className="flex flex-col gap-5 border border-gray-500 p-5 rounded-md">
+            <Image src='https://source.unsplash.com/featured/300x300?' height={300} width={300} alt="imagem"/>
+
+            <div className="flex gap-5 justify-between">
+                {renderizarBotao('Estudar')}
+                {renderizarBotao('Ler')}
+                {renderizarBotao('Praticar')}
+            </div>
+
         </div>
     )
 }
--- inicial-app/src/app/pages/hooks/imagens.tsx
+++ inicial-app/src/app/pages/hooks/imagens.tsx
@@ -2,7 +2,7 @@ import ImagemAleatoria from "@/app/components/hooks/ImagemAleatoria"
 
 export default function PaginaImagem() {
     return (
-        <div className="flex justify-center gap-5 bg-purple-600 ">
+        <div className="flex justify-center gap-5 ">
             <ImagemAleatoria />
             <ImagemAleatoria />
             <ImagemAleatoria />
